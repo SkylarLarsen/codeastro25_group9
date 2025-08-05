f61ae69821c51aa3106cadee15983b16e2e40275
Skylar Larsen <skylarlarsen@gmail.com>
2025-08-05 18:00:21 -0500

    add habitable zone visualization function

--- plotting.py
+++ plotting.py
@@ -1,40 +1,13 @@
-
-from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive
-from astroquery.simbad import Simbad
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
-def get_current_parameters(planet_name='Kepler-22 b'):
-  '''
-  Returns:
-  Dictionary of stellar parameters. Keys in dictionary include:
-  Planet Mass [Msun]
-  T_eff of star [K]
-  Luminosity of Star [Lsun]
-  Radius of star [Rsun]
+d_TEST = {'time': pd.Series([0, 1, 2, 3]),
+     'distance_hz_in': pd.Series([0.8, 0.8, 0.9, 1.2]),
+     'distance_hz_out': pd.Series([1, 1, 1.1, 1.5]),
+                      }
 
-  Args:
-  name_planet: name of planet in nasa exoplanet archive (string)
-  name_star: name of star in Simbad (string)
-  '''
-  tab = NasaExoplanetArchive.query_criteria(table="pscomppars", where=f"pl_name='{planet_name}'").to_pandas()
-  dict = tab.to_dict(orient='records')[0]
-  #star_dict = star_tab.to_dict(orient='records')[0]
-  return dict
-
-# def evolve_stellar_parameters(stellar_parameters: dict, current_age, target_age) -> dict:
-
-#   return aged_stellar_parameters
-
-
-# def find_habitable_zone(st_teff, st_lum, pl_mass):
-
-#   inner = 0
-#   outer = 0
-  
-#   bounds = [inner, outer]
-#   return bounds
+df_TEST = pd.DataFrame(d_TEST)
 
 def visualize(df, time_bc, distance_bc, planet_AU):
     """
@@ -72,3 +45,6 @@ def visualize(df, time_bc, distance_bc, planet_AU):
     plt.ylabel("Distance from Star (AU)")
 
     plt.show()
+
+
+TEST_plot = visualize(df_TEST, [0,4], [0.5,1.7], [0.7, 1.1, 1.5])
