3f0186421a0a1c9b44e63f9cca9a41f74e5a72f3
danyaalb <alboslani.danya@gmail.com>
2025-08-05 17:57:51 -0500

    dictionary only

--- core.py
+++ core.py
@@ -17,25 +17,9 @@ def get_current_parameters(planet_name='Kepler-22 b'):
   name_star: name of star in Simbad (string)
   '''
   tab = NasaExoplanetArchive.query_criteria(table="pscomppars", where=f"pl_name='{planet_name}'").to_pandas()
-  dict = planet_tab.to_dict(orient='records')[0]
+  dict = tab.to_dict(orient='records')[0]
   #star_dict = star_tab.to_dict(orient='records')[0]
   return dict
-
-  # pl = Planet(name=comp_dict['pl_name'], properties=comp_dict)
-
-  # table = NasaExoplanetArchive.query_criteria(table=table, where=f"hostname='{star_name}' and st_teff IS NOT null")
-  # try:
-  #     t_dict = table.to_pandas().to_dict(orient='records')[0]
-  # except IndexError:
-  #     raise ValueError(f"No IPAC stellar entries found for {star_name}")
-  # try:
-  #     spec_type = Simbad.query_tap(f"SELECT main_id, sp_type FROM basic WHERE main_id = '{star_name}'")['sp_type'][0]
-  #     t_dict['st_spectype_simbad'] = spec_type
-  # except Exception as e:
-  #     spec_type = None
-  #     t_dict['st_spectype_simbad'] = spec_type
-    # star = Star(name=star_name, properties=t_dict)
-  #, t_dict
 
 # def evolve_stellar_parameters(stellar_parameters: dict, current_age, target_age) -> dict:
 
