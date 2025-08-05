1a9697dbfb478833e9004fdeb4077cc163ae192b
Nick Marston <ntmarston@gmail.com>
2025-08-05 14:31:30 -0500

    Create core.py
    Outline code

---
+++ core.py
@@ -0,0 +1,23 @@
+# The "main" file, e.g. the one that either contains or points to all the other ones
+
+#determine how the star will be identified
+def get_current_stellar_parameters(star_identifier) -> dict:
+  return stellar_parameters_dict
+
+
+def evolve_stellar_parameters(stellar_parameters: dict, current_age, target_age) -> dict:
+
+  return aged_stellar_parameters
+
+
+def find_habitable_zone(st_teff, st_lum, pl_mass):
+
+  inner = 0
+  outer = 0
+  
+  bounds = [inner, outer]
+  return bounds
+
+def visualize():
+  #plot results
+  return 
