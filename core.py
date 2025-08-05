cca0b01239b0669fafb5a810a3d0fbd09999ce0b
danyaalb <alboslani.danya@gmail.com>
2025-08-05 17:24:03 -0500

    only querying planet

--- core.py
+++ core.py
@@ -16,30 +16,26 @@ def get_current_parameters(planet_name='Earth',star_name='Sun'):
   name_planet: name of planet in nasa exoplanet archive (string)
   name_star: name of star in Simbad (string)
   '''
-  composite_set = NasaExoplanetArchive.query_criteria(table="pscomppars", where=f"pl_name='{planet_name}'")
+  composite_set = NasaExoplanetArchive.query_criteria(table="pscomppars", where=f"pl_name='{planet_name}'")#where=f"disc_facility like'{planet_name}'"
   comp_df = composite_set.to_pandas()
-  comp_dict = comp_df.to_dict(orient='records')[0]
+  #comp_dict = comp_df.to_dict(orient='records')[0]
+  return comp_df
 
   # pl = Planet(name=comp_dict['pl_name'], properties=comp_dict)
 
-  table = NasaExoplanetArchive.query_criteria(table=table, where=f"hostname='{star_name}' and st_teff IS NOT null")
-  try:
-      t_dict = table.to_pandas().to_dict(orient='records')[0]
-  except IndexError:
-      raise ValueError(f"No IPAC stellar entries found for {star_name}")
-  try:
-      spec_type = Simbad.query_tap(f"SELECT main_id, sp_type FROM basic WHERE main_id = '{star_name}'")['sp_type'][0]
-      t_dict['st_spectype_simbad'] = spec_type
-  except Exception as e:
-      spec_type = None
-      t_dict['st_spectype_simbad'] = spec_type
+  # table = NasaExoplanetArchive.query_criteria(table=table, where=f"hostname='{star_name}' and st_teff IS NOT null")
+  # try:
+  #     t_dict = table.to_pandas().to_dict(orient='records')[0]
+  # except IndexError:
+  #     raise ValueError(f"No IPAC stellar entries found for {star_name}")
+  # try:
+  #     spec_type = Simbad.query_tap(f"SELECT main_id, sp_type FROM basic WHERE main_id = '{star_name}'")['sp_type'][0]
+  #     t_dict['st_spectype_simbad'] = spec_type
+  # except Exception as e:
+  #     spec_type = None
+  #     t_dict['st_spectype_simbad'] = spec_type
     # star = Star(name=star_name, properties=t_dict)
-  return comp_dict, t_dict
-
-table_planet,table_star=get_current_parameters()
-
-print(table_planet)
-
+  #, t_dict
 
 # def evolve_stellar_parameters(stellar_parameters: dict, current_age, target_age) -> dict:
 
