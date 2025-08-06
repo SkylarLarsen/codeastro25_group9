e22adbcfa0328155df4493bf9d41888272c227ea
Nick <ntmarston@gmail.com>
2025-08-05 20:28:40 -0500

    Added find_hz method and the helper methods that belong to it

--- core.py
+++ core.py
@@ -97,3 +97,44 @@ def ensure_unit(x, unit: u.Unit):
         except u.UnitConversionError as uce:
             raise u.UnitTypeError(f"{x} cannot be converted to {unit}")
     return x
+
+def dist_from_Seff(Seff, L):
+    """Helper method to convert Seff to distance (AU)"""
+    #L must be in solar units
+    d = (L / Seff) ** 0.5
+    return d
+
+
+def find_hz(st_teff, st_lum):
+    """
+    Todo: write documentation
+    note that rg0.1, rg1, and rg5 correspond to the 0.1, 1, and 5 Earth mass runaway greenhouse values. 
+    """
+
+    def KopparapuEqnFour(SeffSUN, a, b, c, d, tS):
+        Seff = SeffSUN + a * tS + b * ((tS) ** 2) + c * ((tS) ** 3) + d * ((tS) ** 4)
+        return Seff
+    
+    L = ensure_unit(st_lum, u.Lsun)
+    T_s = ensure_unit(st_teff, u.K) - 5780 * u.K
+    
+    recent_venus = {'label': 'rv', 'Seff': 1.776000 , 'a': 2.136000e-04 , 'b': 2.533000e-08, 'c': -1.33200e-11, 'd': -3.09700e-15}
+    runaway_greenhouse_1Mearth = {'label': 'rg1', 'Seff': 1.107, 'a': 1.332000e-04 , 'b': 1.580000e-08, 'c': -8.30800e-12, 'd': -1.93100e-15}
+    maximum_greenhouse = {'label': 'mxg', 'Seff': 3.560000e-01, 'a': 6.171000e-05 , 'b': 1.698000e-09, 'c': -3.19800e-12, 'd': -5.57500e-16}
+    early_mars = {'label': 'em', 'Seff': 3.200000e-01, 'a': 5.547000e-05 , 'b': 1.526000e-09, 'c': -2.87400e-12, 'd': -5.01100e-16}
+    runaway_greenhouse_5Mearth = {'label': 'rg5', 'Seff': 1.188000, 'a': 1.433000e-04 , 'b': 1.707000e-08, 'c': -8.96800e-12, 'd': -2.08400e-15}
+    runaway_greenhouse_01Mearth = {'label': 'rg0.1', 'Seff': 9.900000e-01, 'a': 1.209000e-04 , 'b': 1.404000e-08, 'c': -7.41800e-12, 'd': -1.71300e-15}
+    
+    coeff_matrix = pd.DataFrame([recent_venus, runaway_greenhouse_01Mearth, runaway_greenhouse_1Mearth, 
+                                 runaway_greenhouse_5Mearth, maximum_greenhouse, early_mars])
+    coeff_matrix.set_index('label', inplace=True)
+
+    #Add column with the result of eqn. 4 in Kopparapu 2014
+    for index, row in coeff_matrix.iterrows():
+        #todo carry units through
+        SeffBound = KopparapuEqnFour(row['Seff'], row['a'], row['b'], row['c'], row['d'], T_s.value)
+        coeff_matrix.at[index,'SeffBound'] = SeffBound
+        coeff_matrix.at[index,'distBound(AU)'] = dist_from_Seff(SeffBound, L.value)
+        
+    
+    return coeff_matrix
