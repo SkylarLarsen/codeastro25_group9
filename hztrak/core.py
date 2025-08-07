69215e54f5a9e529f7634faadf216db1a6c9463e
danyaalb <alboslani.danya@gmail.com>
2025-08-07 15:24:17 -0500

    dataframe to astropy tables in get_current_parameters. Tested properly

--- hztrak/core.py
+++ hztrak/core.py
@@ -6,8 +6,9 @@ from astropy.constants import R_earth, M_earth, R_sun, M_sun, G
 
 import pandas as pd
 import matplotlib.axes
-from astropy import Table
+from astropy.table import Table,vstack
 import matplotlib.pyplot as plt
+ 
 
 
 def get_current_parameters(planet_name=['Kepler-22 b']):
@@ -23,33 +24,32 @@ def get_current_parameters(planet_name=['Kepler-22 b']):
   Returns:
   Pandas dataframe: Planet names and parameters for the planet and host star
   '''
-  data=[]
+  table_list=[]
   for i in range(len(planet_name)):
-    tab = NasaExoplanetArchive.query_criteria(table="pscomppars", where=f"pl_name='{planet_name[i]}'").to_pandas()
+    tab = NasaExoplanetArchive.query_criteria(table="pscomppars", where=f"pl_name='{planet_name[i]}'")
     if len(tab)==0:
-      data.append({'pl_name':planet_name[i],           #planet name
-
-      })
-      continue
+        print(f'{planet_name[i]} not found! Try again bestie :/')
+        continue
     else:
-      planet_dict = tab.to_dict(orient='records')[0]
-      data.append({'pl_name':planet_dict['pl_name'],           #planet name
-          'hostname':planet_dict['hostname'],       #host star name
-          'pl_rade': planet_dict['pl_rade'],          #planet radius [earth radius]
-          'pl_masse': planet_dict['pl_masse'] ,        #planet mass [earth mass]
-          'pl_ratror': planet_dict['pl_ratror'],        #Ratio of Planet to Stellar Radius
-          'st_teff':planet_dict['st_teff'],          #stellar effective temperature [K]
-          'st_rad':planet_dict['st_rad'],           #stellar radius [Rsun]
-          'st_mass':planet_dict['st_mass'],          #stellar mass [Msun]
-          'st_lum':planet_dict['st_lum'],           #Stellar Luminosity [log10(Solar)]
-          'st_age' :planet_dict['st_age'],          #stellar age [Gyr]
-          'pl_orbper':planet_dict['pl_orbper'],        #Orbital period [days]
-          'pl_orbsmax':planet_dict['pl_orbsmax'],       #Orbit Semi-Major Axis [au]
-          })
-  df=pd.DataFrame(data)
-  tab = Table.from_pandas(df)
-      
-  return tab
+        table_list.append(tab)
+    final_tab=vstack(table_list)
+    #   planet_dict = tab.to_dict(orient='records')[0]
+    #   data.append({'pl_name':planet_dict['pl_name'],           #planet name
+    #       'hostname':planet_dict['hostname'],       #host star name
+    #       'pl_rade': planet_dict['pl_rade'],          #planet radius [earth radius]
+    #       'pl_bmasse': planet_dict['pl_masse'] ,        #planet mass [earth mass]
+    #       'pl_ratror': planet_dict['pl_ratror'],        #Ratio of Planet to Stellar Radius
+    #       'st_teff':planet_dict['st_teff'],          #stellar effective temperature [K]
+    #       'st_rad':planet_dict['st_rad'],           #stellar radius [Rsun]
+    #       'st_mass':planet_dict['st_mass'],          #stellar mass [Msun]
+    #       'st_lum':planet_dict['st_lum'],           #Stellar Luminosity [log10(Solar)]
+    #       'st_age' :planet_dict['st_age'],          #stellar age [Gyr]
+    #       'pl_orbper':planet_dict['pl_orbper'],        #Orbital period [days]
+    #       'pl_orbsmax':planet_dict['pl_orbsmax'],       #Orbit Semi-Major Axis [au]
+    #       })
+#   df=pd.DataFrame(data)
+#   tab = Table.from_pandas(df)
+  return final_tab
 
 def visualize(df, time_bc, distance_bc, planet_AU):
     """Visualization_1
